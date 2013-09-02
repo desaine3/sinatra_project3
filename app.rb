4403ca480a66b691d1be0cfd7d7228ab447a2461
Elina Desaine <edesaine@ticklo.com>
2013-09-02 17:31:15 +0100

    a

--- app.rb
+++ app.rb
@@ -6,5 +6,22 @@ def get_data
 end
 
 get '/' do 
-    @data = data.to_a
+  get_data.to_a
 end
+
+def text
+    File.open('cfg_profiles.csv', 'r') do |f|
+        puts "Opening 'cfg_profiles.txt'..."
+        raw_text = f.read  # read the contents of the file
+        return raw_text
+    end
+end
+
+def write_html(data)
+    puts "Creating html..."
+
+    puts data
+    puts "Output written. Enjoy!"
+end
+
+write_html(text)
